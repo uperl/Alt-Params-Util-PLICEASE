45082318ab21d70550c52269102fee6b3a56896b
Graham Ollis <plicease@cpan.org>
2026-02-07 10:59:10 -0700

    Initial structure

---
+++ lib/Alt/Params/Util/PLICEASE.pm
@@ -0,0 +1,9 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+use true;
+
+package Alt::Params::Util::PLICEASE {
+
+    # ABSTRACT: Simple, compact and correct param-checking functions
+}
