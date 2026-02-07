ff1c8480936793f2d4d89a82bf3d26b6b2eaf137
Graham Ollis <gollis@fastly.com>
2026-02-07 11:50:39 -0700

    impl

--- lib/Alt/Params/Util/PLICEASE.pm
+++ lib/Alt/Params/Util/PLICEASE.pm
@@ -3,8 +3,29 @@ package Alt::Params::Util::PLICEASE;
 # ABSTRACT: Simple, compact and correct param-checking functions
 # VERSION
 
-use warnings;
 use 5.020;
-use experimental qw( postderef signatures );
+use warnings;
 use true;
 
+=head1 SYNOPSIS
+
+env PERL_ALT_INSTALL=OVERWRITE cpanm Alt::Params::Util::PLICEASE
+
+=head1 DESCRIPTION
+
+This module provides an alternate implementation for L<Params::Util>.
+It will only be installed if you explicitly set C<PERL_ALT_INSTALL>
+to C<OVERWRITE>.  L<Params::SomeUtil> has details on how the two
+implementations differ.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Alt>
+
+=item L<Params::SomeUtil>
+
+=back
+
+=cut
--- lib/Params/Util.pm
+++ lib/Params/Util.pm
@@ -1,8 +1,34 @@
 package Params::Util;
 
-use strict;
+# ABSTRACT: Simple, compact and correct param-checking functions
+
 use 5.020;
+use warnings;
 use true;
 use Params::SomeUtil;
 
 Params::SomeUtil->_alt_hook;
+
+=head1 SYNOPSIS
+
+ use Params::Util;
+
+=head1 DESCRIPTION
+
+This module is a shim that replaces the official L<Params::Util> with L<Params::SomeUtil>.
+If it is installed on your system, then at some point L<Alt::Params::Util::PLICEASE> was
+installed.
+
+For details on how this version differs from the original please see L<Params::SomeUtil>.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Alt>
+
+=item L<Params::SomeUtil>
+
+=back
+
+=cut
